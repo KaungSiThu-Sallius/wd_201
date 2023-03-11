3fde94661703c5fa00a9f1560b3d95a88c181a90
KaungSiThu-Sallius <kaungsithu.sallius@gmail.com>
2023-03-11 23:09:41 +0530

    eslint add

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+const todoList = require("../todo");
+
+const { all, markAsComplete, add } = todoList();
+
+describe("Todolist Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString("en-CA"),
+    });
+  });
+  test("should add new todo", () => {
+    const toDoLength = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString("en-CA"),
+    });
+    expect(all.length).toBe(toDoLength + 1);
+  });
+
+  test("should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
