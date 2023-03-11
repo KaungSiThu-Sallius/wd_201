a764a43cc5e4202be338d42cb9febf0eae43531e
KaungSiThu-Sallius <kaungsithu.sallius@gmail.com>
2023-03-11 23:52:07 +0530

    add date test

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,7 +1,14 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
+const todos = todoList();
 const { all, markAsComplete, add } = todoList();
+
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+
+var dateToday = new Date();
 
 describe("Todolist Test Suite", () => {
   beforeAll(() => {
@@ -25,5 +32,42 @@ describe("Todolist Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("should retrieval of overdue items", () => {
+    add({
+      title: "Test todo yesterday",
+      completed: false,
+      dueDate: formattedDate(
+        new Date(new Date().setDate(dateToday.getDate() - 1))
+      ),
+    });
+    var overdues = todos.overdue();
+
+    expect(overdues.length).toBeGreaterThan(0);
+  });
+
+  test("should retrieval of due today items", () => {
+    add({
+      title: "Test todo due today",
+      completed: false,
+      dueDate: formattedDate(dateToday),
+    });
+    var dueToday = todos.dueToday();
+
+    expect(dueToday.length).toBeGreaterThan(0);
+  });
+
+  test("should retrieval of due later items", () => {
+    add({
+      title: "Test todo due later",
+      completed: false,
+      dueDate: formattedDate(
+        new Date(new Date().setDate(dateToday.getDate() + 1))
+      ),
+    });
+    var dueLater = todos.dueLater();
+
+    expect(dueLater.length).toBeGreaterThan(0);
   });
 });
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -82,18 +82,18 @@ module.exports = todoList;
 
 // const todos = todoList();
 
-// const formattedDate = d => {
-//     return d.toISOString().split("T")[0]
-// }
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
 
-// var dateToday = new Date()
-// const today = formattedDate(dateToday)
-// const yesterday = formattedDate(
-//     new Date(new Date().setDate(dateToday.getDate() - 1))
-// )
-// const tomorrow = formattedDate(
-//     new Date(new Date().setDate(dateToday.getDate() + 1))
-// )
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+);
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+);
 
 // todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
 // todos.add({ title: 'Pay rent', dueDate: today, completed: true })
